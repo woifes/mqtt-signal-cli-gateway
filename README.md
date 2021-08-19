9979bf4ddb424f2489db61ca3d9a228fbe8ce9e6
woifes <81268775+woifes@users.noreply.github.com>
2021-08-19 20:14:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# MQTT - Signal-Cli Gateway
+
+This is a lightweight, single file mqtt to signal-cli gateway written in python. It is assumed that signal-cli is running as a username independent dbus service (without the "-u" option) on the system. The gateway connects to the service and sends messages from signal to the topic 
+```
+signal/from/<phonenumberwithout+sign
+```
+Mqtt publishes to the optic 
+```
+signal/to/<phonenumberwithout+sign>
+```
+are send to the corresponding number via signal. For security it has to be set a set of "known numbers" which are allowed to participate to the whole process.
+
+Additionally there is a ```/help``` which atm only shows the command ```/ping``` which sends a publish to the topic
+```
+signal/ping
+```
+with the ping requester phone number as payload. The gateway subscribes to this topic too and sends back a chat message ```/pong``` if it receives the sent message back (lightweight application level ping)
+
+Lastly the Gateway will publish a message "1" to
+```
+signal/<clientId>
+```
+when connecting and a will message "0" to the same topic, to make the presence visible on mqtt level. This can be prevented by the corresponding parameter
+
+## Installation
+* Get signal-cli going on your system and start it as a dbus service (https://github.com/AsamK/signal-cli/wiki/DBus-service)
+* make sure the number you want to use is registered and the dbus service can handle it
+* Put the files where you need them
+    * ```main.py``` wherever you want (has to be executable)
+    * ```app.ini``` wherever you want (see example file)
+    * ```mqtt-signal-gateway.service``` under ```/etc/systemd/system``` (adjust the paths in the service file to your need)
+* enable and start the service
+    * ```systemctl enable mqtt-signal-gateway```
+    * ```systemctl start mqtt-signal-gateway```
+
+## Dependencies
+* signal-cli running as a phone number (user) independent dbus service
+* registered phone number via signal-cli
+* python3
+    * pydbus
+    * paho-mqtt
+    * GLib
+
+## Special thanks
+To AsamK for creating signal-cli (https://github.com/AsamK/signal-cli)
---
+++ main.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+from pydbus import SystemBus
+import paho.mqtt.client as mqtt
+from gi.repository import GLib
+import configparser
+import sys
+
+config = configparser.ConfigParser()
+config.read(sys.argv[1])
+
+knownNumbers = config["signal"]["knownNumbers"].replace(" ", "").split(",")
+ownNumber = config["signal"]["ownNumber"]
+
+mqttConfig = {
+	"host": config["mqtt"]["host"],
+	"port": int(config["mqtt"]["port"]),
+	"clientId": "signalGateway" + ownNumber[-3:],
+	"username": config["mqtt"]["username"],
+	"password": config["mqtt"]["password"],
+	"caCert": config["mqtt"]["caCert"],
+	"notifyPresence": True if config["mqtt"]["notifyPresence"] == "1" else False
+}
+
+class MqttSignal:
+	def __init__(self):
+		self._bus = SystemBus()
+		self._signal = self._bus.get('org.asamk.Signal', '/org/asamk/Signal/' + ownNumber.replace("+", "_")) #('org.asamk.Signal', '/org/asamk/Signal/_number')
+		self._signal.onMessageReceived = self._onSignalMsg
+		c = {}
+		c["/help"] = { "fn": self._onSignalHelp, "info": "Shows all commands available" }
+		c["/ping"] = { "fn": self._onMqttPing, "info": "Sends mqtt publish and returns a /pong if it was received back" }
+		self._signalCommands = c
+
+		#mqtt
+		self._mqtt = mqtt.Client(mqttConfig["clientId"])
+		self._mqtt.tls_set(mqttConfig["caCert"])
+		self._mqtt.tls_insecure_set(True)
+		self._mqtt.username_pw_set(mqttConfig["username"], mqttConfig["password"])
+		if mqttConfig["notifyPresence"]:
+			self._mqtt.will_set("clients/" + mqttConfig["clientId"], "0", 0, True)
+		self._mqtt.on_connect = self._onMqttConnect
+		self._mqtt.on_message = self._onMqttMessage
+		self._mqtt.connect_async(mqttConfig["host"], mqttConfig["port"])
+
+	#signal
+
+	def _sendSignalMsg(self, message, to):
+		self._signal.sendMessage(message, [], to)
+
+	def _onSignalMsg(self, timestamp, source, groupID, message, attachments):
+		if(not source in knownNumbers):
+			return
+
+		if(len(message) == 0): #happens if you send a attachment without text
+			return
+		elif(message[0] == "/"):
+			args = message.split(" ")
+			cmd = args[0]
+			if(cmd in self._signalCommands):
+				self._signalCommands[cmd]["fn"](timestamp, source, args[1:])
+			else:
+				self._sendSignalMsg('Use /help to get information about the provided commands', source)
+		else:
+			self._publishMqtt("signal/from/" + source[1:], message)
+		
+	def _onSignalHelp(self, timestamp, source, params):
+		m = ""
+		for c in self._signalCommands:
+			m += c + " - " + self._signalCommands[c]["info"] + "\n"
+		self._sendSignalMsg(m, source)
+
+	def _onMqttPing(self, timestamp, source, params):
+		self._publishMqtt("signal/ping", source)
+	
+	#mqtt
+
+	def _publishMqtt(self, topic, message, qos=0, retain=False):
+		self._mqtt.publish(topic, message, qos, retain)
+
+	def _onMqttConnect(self, client, userdata, flags, rc):
+		if mqttConfig["notifyPresence"]:
+			self._publishMqtt("clients/" + mqttConfig["clientId"], "1", 0, True)
+		self._mqtt.subscribe("signal/ping", 2)
+		for n in knownNumbers:
+			self._mqtt.subscribe("signal/to/" + n[1:])
+	
+	def _onMqttMessage(self, client, userdata, m):
+		m.payload = m.payload.decode("UTF-8")
+		if(m.topic == "signal/ping"):
+			self._onMqttPong(m)
+		elif(m.topic.find("signal/to/") != -1):
+			receiver = "+" + m.topic.split("/")[-1:][0]
+			if(receiver in knownNumbers):
+				self._sendSignalMsg(m.payload, receiver)
+
+	def _onMqttPong(self, m):
+		if(m.payload in knownNumbers):
+			self._sendSignalMsg("/pong", m.payload)
+
+	def loop(self):
+		self._mqtt.loop_start()
+
+	
+MQTTSIGNAL = MqttSignal()
+MQTTSIGNAL.loop()
+loop = GLib.MainLoop()
+loop.run()
